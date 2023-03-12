fde04a1bce7869470f2f67b13e7a69bca04bea84
onlywjt <1468782472@qq.com>
2023-03-12 18:00:08 +0800

    单文件组件以及脚手架

---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo">
+    <!-- 组件的结构 -->
+    <h2>学生名称:{{ name }}</h2>
+    <h2>年龄:{{ address }}</h2>
+    <!-- <button @click="showName">点我提示学校名称</button> -->
+  </div>
+</template>
+<script>
+export default {
+  name: 'Student',
+  data () {
+    return {
+      name: '天津城建大学',
+      age: 21
+    }
+  },
+  methods: {
+    showName () {
+      alert(this.schoolName);
+    }
+  }
+}
+//统一暴露
+// export {
+//   school
+// }
+</script>
+
+<style>
+</style>
