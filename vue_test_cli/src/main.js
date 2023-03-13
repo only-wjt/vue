4471e4568214acebff927aad84af8d108cf5f934
onlywjt <1468782472@qq.com>
2023-03-13 21:38:42 +0800

    biandong

--- vue_test_cli/src/main.js
+++ vue_test_cli/src/main.js
@@ -18,7 +18,7 @@ import Vue from 'vue'
 Vue.config.productionTip = false
 //创建Vue实例对象
 new Vue({
-  // el: '#app',
+  // el: '#app',  
   //将app组件放入容器中
   // template: `<h1>你好</h1>`,
   // components: {
