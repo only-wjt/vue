d097ae73da8a630bb1f4f57eceba737555c112c1
onlywjt <1468782472@qq.com>
2023-03-26 18:00:38 +0800

    onlyWjt 20230326 vue中的vuex插件，使用 npm i vuex@3下载之后，在src目录下创建strore/index.js文件，然后引入vue， new Vuex.Store创建strore后使用

--- vue_test_cli/23_src_求和案例-vue版本/main.js
+++ vue_test_cli/23_src_求和案例-vue版本/main.js
@@ -2,23 +2,14 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-//引入插件
-// import vueResource from 'vue-resource'
-//引入store
-import store from './store/index'
-
 //关闭Vue的生产提示
 Vue.config.productionTip = false
-//使用插件
-// Vue.use(vueResource)
 
 //创建vm
-const vm = new Vue({
+new Vue({
 	el: '#app',
 	render: h => h(App),
-	store,
 	beforeCreate () {
 		Vue.prototype.$bus = this
-	}
+	},
 })
-console.log('@', vm);
--- vue_test_cli/src/App.vue
+++ vue_test_cli/src/App.vue
@@ -1,50 +1,16 @@
 <template>
-  <div class="container">
-    <Category title="游戏">
-      <template v-slot="atguigu">
-        <ul>
-          <li v v-for="(g, index) in atguigu.games" :key="index">{{ g }}</li>
-        </ul>
-      </template>
-    </Category>
-    <Category title="游戏">
-      <template v-slot="atguigu">
-        <ol>
-          <li v v-for="(g, index) in atguigu.games" :key="index">{{ g }}</li>
-        </ol>
-        <h1>{{ atguigu.msg }}</h1>
-      </template>
-    </Category>
-    <Category title="游戏">
-      <template v-slot="{ games }">
-        <h4 v v-for="(g, index) in games" :key="index">{{ g }}</h4>
-      </template>
-    </Category>
+  <div>
+    <Count />
   </div>
 </template>
 
 <script>
-import Category from './components/Category.vue'
+import Count from './components/Count'
 export default {
   name: 'App',
-  components: { Category }
-
+  components: { Count },
+  mounted () {
+    // console.log('App',this)
+  },
 }
 </script>
-<style lang="css">
-.container,
-.foot {
-  display: flex;
-  justify-content: space-around;
-}
-h3 {
-  text-align: center;
-  background-color: orange;
-}
-img {
-  width: 100%;
-}
-h4 {
-  text-align: center;
-}
-</style>
